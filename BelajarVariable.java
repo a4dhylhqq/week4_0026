83b598aee0d4ba1be74e5e528ba747138835823b
a4dhylhqq <ahmaddhiyaul34@gmail.com>
2025-10-22 15:46:36 +0700

    declare variable

--- BelajarVariable.java
+++ BelajarVariable.java
@@ -1,6 +1,13 @@
+
+import java.util.Scanner;
+
 public class BelajarVariable{
     
     public static void main(String[] args){
-
+        int nUmur;
+        Scanner ns = new Scanner(System.in);
+        
+        System.err.println("Masukkan umur");
+        nUmur = ns.nextInt();
     }
 }
